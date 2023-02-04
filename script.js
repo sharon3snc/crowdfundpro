5197deb4ea80cd65f251d040b12f04dd91f9f29d
sharon3snc <sharonnunez89@gmail.com>
2023-02-04 12:51:07 +0100

    ok1

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
-let nombre, donacion, suma, comentario;
 
-//acumula donaciones
-function acumula (){
-    var suma=0;
-    var donacion= 0;
-
-    donacion= parseInt("donacion");
-    suma= suma+ donacion;
-}
-
-//contador de donantes
-var cont=0;
-var pvalor= document.getElementById("valor");
-var btnConfirmar= document.getElementById("confirmar")
-
-btnConfirmar.onclick = function (){
-    cont++;
-    pvalor.textContent= cont;
-}
-
-
-
-function obtenerdatos(){
+function obtenerdatos() {
+        console.log("---getting data");
+     
         var nombre= document.getElementById ("name").value;
         var donacion= document.getElementById ("aporte").value;
         var comentario= document.getElementById ("cuentame").value;
 
+        console.log(nombre+ " " + donacion+ " " + comentario +". ");
         let mensajefinal= "Muchas gracias, " + nombre;
         console.log(mensajefinal);
+        window.modal.close(modal);
+}
+
+//muestro comentarios
+function comentario (postList){
+    var HTML= '';
+
+        HTML += `
+        <div class="title">
+        <div> ${"name"} </div>
+        </div>
+        <div class="post">
+        <div> ${"cuentame"} </div>
+        </div>
+        `
+    document.getElementById("postList").innerHTML= HTML;
+}
+
+//Acumula donaciones
+function acumula (){
+    var suma=0;
+    var donacion= 0;
+
+    donacion= parseInt("aporte");
+    suma= suma+ donacion;
+    console.log(suma)
+} 
+
+
+//contador de donantes
+var cont=0;
+
+function contador(){
+    updateDisplay(++cont);
+}
+
+function updateDisplay (val) {
+document.getElementById("valor").innerHTML=val;
 }
 
 
